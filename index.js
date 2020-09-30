2f498565b8319d78c70d07c05fbcb1b86cd0ba96
Himawan Renn <66410293+HimawanRenn@users.noreply.github.com>
2020-09-30 14:21:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('Tersambung.');
 });
-
+// testing bot nya berjalan apa tidak ketik ping di server discord yang ada bot kalian
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
 });
 
-client.login('token');
+client.login('NzU4Njc3NTE0NDI3ODI2MTc4.X2ybfg.U46wFhm1QHFCUsJNVWJcZbwty24');
