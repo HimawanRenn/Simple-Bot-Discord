d8ab9b8dd450acc3ba77348e018d3a29c6e6ee2d
Himawan Renn <66410293+HimawanRenn@users.noreply.github.com>
2020-10-30 10:26:52 +0700

    Update index.js  🚀

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// Bagian ini menunjukan di console bahwa bot yg lu buat udah nyala
 client.on('ready', () => {
-  console.log('Tersambung.');
+  console.log('Bot sudah nyala.');
 });
+
 // testing bot nya berjalan apa tidak ketik ping di server discord yang ada bot kalian
 client.on('message', msg => {
   if (msg.content === 'ping') {
